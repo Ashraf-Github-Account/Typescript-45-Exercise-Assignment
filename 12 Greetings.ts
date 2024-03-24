984524aaaf57c2ab0ee4b5c8c323ee773dbab517
Ashraf-Github-Account <164771322+Ashraf-Github-Account@users.noreply.github.com>
2024-03-24 10:47:37 -0700

    Add files via upload

---
+++ 12 Greetings.ts
@@ -0,0 +1,11 @@
+
+let names :string [ ] = ['Aqeel', 'Waheed' , 'Shoaib', 'Junaid'] ;
+
+const message : string = 'Welcome to Typescript Class: '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names [i] ) ;
+   
+}
+
