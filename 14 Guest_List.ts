3ca8da0b0b1d5bc23fc09d466c27532f1720f11f
Ashraf-Github-Account <164771322+Ashraf-Github-Account@users.noreply.github.com>
2024-03-24 10:50:49 -0700

    Add files via upload

---
+++ 14 Guest_List.ts
@@ -0,0 +1,8 @@
+
+let Guest_List :string [ ] = ['Imran Khan' , 'Nawaz Sharif' , 'Asif Ali Zardari'] ;
+
+for( let i=0 ; i<Guest_List.length ; i++) {
+
+    console.log('Dear Mr.'+ Guest_List [ i ] + ',\n\n It is our pleasure to invit you in our party. \n\n Thank You')
+    
+}
