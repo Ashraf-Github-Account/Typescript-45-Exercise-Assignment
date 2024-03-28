eff2f1471c9600d6b9e73a82e18894a43e71dcf2
Muhammad Ashraf <164771322+Ashraf-Github-Account@users.noreply.github.com>
2024-03-28 13:03:02 -0700

    Add files via upload

---
+++ 20-Empty_Array.ts
@@ -0,0 +1,14 @@
+
+// Think of Something You could store in a Array.
+let item : string[] = ['Pakistan' , 'Karachi' ,'Urdu'];
+
+// Write a program that creates a list containing these items.
+let item2 : string[] = [] ;
+
+item2.push('Germany');
+item2.push('England');
+item2.push('India');
+
+console.log(item2);
+
+
