f03a47c0336d65cb70fb6444abd5483f1311a2cc
Muhammad Ashraf <164771322+Ashraf-Github-Account@users.noreply.github.com>
2024-03-28 14:25:20 -0700

    Add files via upload

---
+++ 3-Name-Cases.ts
@@ -0,0 +1,19 @@
+let personName : string =  '';
+
+personName = prompt ('what is your name') ||  '';
+
+let lowercase : string = personName. toLowerCase() ;
+let upercase  : string = personName. toUpperCase() ;
+let titlecase : string = personName. split(' ').map(word => word.charAt(0).toUpperCase () + word.slice (1).toLowerCase ()).join (' ')
+
+if (personName !== null && personName !== ' '){
+
+alert(`Hellow ${personName},Here are your name in:
+
+LowerCase: ${lowercase}
+UperCase : ${upercase}
+TitleCase: ${titlecase}`)
+}
+else{
+    alert(`please fill your name !`)
+}
