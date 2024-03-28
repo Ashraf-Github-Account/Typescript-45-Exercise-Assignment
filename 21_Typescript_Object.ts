021fa346be3b3b4a58bafd8d0861c1063b540e2f
Muhammad Ashraf <164771322+Ashraf-Github-Account@users.noreply.github.com>
2024-03-28 13:06:26 -0700

    Add files via upload

---
+++ 21_Typescript_Object.ts
@@ -0,0 +1,44 @@
+
+// Write a programe that creates object containing these item.
+
+// Datatypes of Person object
+interface person {
+age : number ,
+name : string ,
+nationality : string ,
+student : Boolean 
+
+}
+
+// Person Objest.
+let person : person = {
+age : 26,
+name: 'Ashraf',
+nationality : 'pakistan',
+student : true
+
+}
+// print person
+console.log(person);
+
+// Datatypes of car object.
+interface car {
+maker : string ,
+color : string ,
+automatic : Boolean
+
+}
+
+// car object
+let car = {
+maker : 'toyota',
+color : 'Black',
+automatic : true
+
+}
+
+// print car
+console.log(car);
+
+
+
